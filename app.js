e552352d4cb270841d58f4852572ab6c912bab2b
naijwu <jaewuchun@gmail.com>
2025-01-18 17:46:41 -0800

    for prod

--- app.js
+++ app.js
@@ -1,57 +1,53 @@
-const dotenv = require('dotenv');
-dotenv.config();
+// const dotenv = require('dotenv');
+// dotenv.config();
 
-const express = require('express');
-var cors = require('cors')
+const express = require("express");
+var cors = require("cors");
 const app = express();
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // dev purposes
-mongoose.set('debug', true);
-
+mongoose.set("debug", true);
 
 // import routes
-const authRoute = require('./routes/auth'); 
-const staffRoute = require('./routes/staff'); 
-const secretariatRoute = require('./routes/secretariat'); 
-const committeeRoute = require('./routes/committee');
-const donate = require('./routes/donation')
-
+const authRoute = require("./routes/auth");
+const staffRoute = require("./routes/staff");
+const secretariatRoute = require("./routes/secretariat");
+const committeeRoute = require("./routes/committee");
+const donate = require("./routes/donation");
 
 // temporary - adding another API for CAHS 2021 IPC LMAO
-const ipcRoute = require('./ipc/routes');
+const ipcRoute = require("./ipc/routes");
 
-
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.DB_CONNECT, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error)); // always, on error
-db.once('open', () => console.log('Connected to database!')) // once, on open
+db.on("error", (error) => console.error(error)); // always, on error
+db.once("open", () => console.log("Connected to database!")); // once, on open
 
 const corsConfig = {
-    origin: process.env.CLIENT_URL,
-    optionsSuccessStatus: 200
-}
+	origin: process.env.CLIENT_URL,
+	optionsSuccessStatus: 200,
+};
 
-app.options('*', cors());
+app.options("*", cors());
 app.use(cors());
 app.use(express.json()); // accept json
 
-
 // use routes
-app.use('/authentication', authRoute); // API routes for Secretariat registrations and All logins
-app.use('/staff', staffRoute); // API routes for Sec to manage their staff accounts
-app.use('/secretariat', secretariatRoute); // API routes for Secretariat (for them to edit their own profiles)
-app.use('/committee', committeeRoute);
+app.use("/authentication", authRoute); // API routes for Secretariat registrations and All logins
+app.use("/staff", staffRoute); // API routes for Sec to manage their staff accounts
+app.use("/secretariat", secretariatRoute); // API routes for Secretariat (for them to edit their own profiles)
+app.use("/committee", committeeRoute);
 //API route for stripe integration
-app.use('/donate', donate);
-
-
+app.use("/donate", donate);
 
 // Temporary
-app.use('/ipc', ipcRoute);
-
+app.use("/ipc", ipcRoute);
 
 const port = process.env.PORT || 8080; // can do 'export PORT=X' to change env variable
 app.listen(port, () => {
-    console.log(`Server running on port ${port}! (CORS-enabled)`)
+	console.log(`Server running on port ${port}! (CORS-enabled)`);
 });
